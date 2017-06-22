1c31899828edb226e408c5acdb5c8c21a8a286a8
Sokol V <1470715@gmail.com>
2017-06-22 22:53:44 +0300

    First travel commit

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -4,13 +4,13 @@ module.exports = function(){
 	$.gulp.task('sprite:png', function(){
 		var spriteData = $.gulp.src('./source/sprite/*.+(png|PNG)')
 			.pipe($.gp.spritesmith({
-				retinaSrcFilter: ['./source/sprite/*@2x.+(png|PNG)'],
-				retinaImgName: 'sprite@2x.png',
+				//retinaSrcFilter: ['./source/sprite/*@2x.+(png|PNG)'],
+				//retinaImgName: 'sprite@2x.png',
 				imgName: 'sprite.png',
 				cssName: 'sprite.scss',
 				algorithm: 'top-down',
 				imgPath: '../img/sprite.png',
-				retinaImgPath: '../img/sprite@2x.png'
+				//retinaImgPath: '../img/sprite@2x.png'
 			}));
 
 		var imgStream = spriteData.img
