adef0a292fc816e3737250a41a238ec02e7fd9e6
Sokol V <1470715@gmail.com>
2017-07-04 23:11:09 +0300

    add header

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,9 @@
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+$(document).ready(function(){
+	$('.menu-link').on('click', function(){
+		$(this).toggleClass('menu-link-active');
+	});
+});
