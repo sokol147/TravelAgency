8b888c0f7fe158e2ca0b65790a4e299a34789efd
Sokol V <1470715@gmail.com>
2017-07-05 22:57:28 +0300

    Add menu

--- source/js/app.js
+++ source/js/app.js
@@ -7,7 +7,15 @@
 })();
 
 $(document).ready(function(){
-	$('.menu-link').on('click', function(){
+
+	$('.menu-link').on('click', function(e){
+		e.preventDefault();
 		$(this).toggleClass('menu-link-active');
+		$('.menu').toggleClass('menu-active');
+	});
+	$('.menu-list-item--link').on('click', function(e){
+		e.preventDefault();
+		$('.menu').removeClass('menu-active');
+		$('.menu-link').removeClass('menu-link-active');
 	});
 });
