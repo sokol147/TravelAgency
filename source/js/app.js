7a433fda285eeefe1475fd7878cb4b34eaf0d481
Sokol V <1470715@gmail.com>
2017-08-08 22:45:27 +0300

    finish footer

--- source/js/app.js
+++ source/js/app.js
@@ -90,4 +90,40 @@ $(document).ready(function(){
 		dots: true
 	});
 
+	$('.carousel-partners').owlCarousel({
+		items: 4,
+		smartSpeed: 700,
+		loop: true,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0:{
+				items: 1
+			},
+			768:{
+				items: 2
+			},
+			992:{
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+	$(window).on('scroll', function(){
+		if($(this).scrollTop() > $(this).height()){
+			$('.scroll-top').addClass('active');
+		} else {
+			$('.scroll-top').removeClass('active');
+		}
+	});
+
+	$('.scroll-top').on('click',function(){
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 });
